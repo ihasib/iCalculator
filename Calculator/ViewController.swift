b80e074303a6c980ffca750e5c7d54118a600e1b
S. M. Hasibur Rahman <hasib.jhd@gmail.com>
2020-03-03 23:21:07 +0600

    [Calculator] Basic UI Layout done
    [Details] Basic UI Layout done with 0button functionality
    
    [Developer] S. M. Hasibur Rahman
    [Type] Feature Implemention

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var stackViewHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var clearButton: UIButton!
+    
+    @IBOutlet weak var zeroButton: UIButton!
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBAction func increaseStackHeight(_ sender: Any) {
+        stackViewHeight.constant = stackViewHeight.constant + 50
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func zeroButtonTapped(_ sender: Any) {
+        displayLabel.text = "0"
+    }
 }
 
